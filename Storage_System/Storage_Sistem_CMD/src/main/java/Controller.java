a0ea811d260017188845a600e4b5ad0a8f5e3cd4
Nicholas Monteiro <nick.developerdev@gmail.com>
2025-05-28 15:49:20 -0300

    System storage
    Signed-off-by: Nicholas Monteiro <nick.developerdev@gmail.com>

---
+++ Storage_System/Storage_Sistem_CMD/src/main/java/Controller.java
@@ -0,0 +1,64 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Controller {
+
+    List<Product> products;
+
+    public Controller() {
+        products = new ArrayList<>();
+    }
+
+    // --- Métodos de Operação ---
+
+    public void addProduct(Product product) {
+        if(product != null) {
+            products.add(product);
+            System.out.println("Product " + product.getName() + " added with success!");
+        }else {
+            System.out.println("Error adding product!");
+        }
+    }
+
+    public void removeProduct(int productID) {
+        Product productToRemove = null;
+        for(Product p : products){
+            if(p.getId() == productID){
+                productToRemove = p;
+                break;
+            }
+        }
+        if(productToRemove != null){
+            products.remove(productToRemove);
+            System.out.println("Product " + productToRemove.getName() + " removed with success!");
+        }else {
+            System.out.println("Product not found!");
+        }
+    }
+
+    public void localizeProduct(int productID){
+        Product productToLocate = null;
+        for (Product l : products){
+            if(l.getId() == productID){
+                productToLocate = l;
+                break;
+            }
+        }
+        if(productToLocate != null){
+            System.out.println("Product found!");
+            System.out.println("Name: " + productToLocate.getName());
+            System.out.println("ID: " + productToLocate.getId());
+        }else {
+            System.out.println("Product not found!");
+        }
+    }
+
+    int sequence = 0;
+    public void listProducts(){
+        for (Product p : products){
+            sequence++;
+            System.out.println(sequence + "." + p.getName());
+        }
+    }
+
+}
---
+++ Storage_System/Storage_Sistem_CMD/src/main/java/Main.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Controller con = new Controller();
+
+        System.out.println("\n----Storage System----");
+
+        boolean running = true;
+        while (running){
+            System.out.println("-------------------------\n");
+            System.out.println("Select Options");
+            System.out.println("1 - Add Product");
+            System.out.println("2 - Remove Product");
+            System.out.println("3 - Locate Product");
+            System.out.println("4 - List Products");
+            System.out.println("5 - Exit");
+            System.out.println("-------------------------");
+            System.out.print("Enter option: ");
+
+            int option = in.nextInt();
+            switch (option){
+                case 1:
+                    System.out.println("\n-------------------------\n");
+                    System.out.print("Enter product ID: ");
+                    int id = in.nextInt();
+                    System.out.println("\n-------------------------\n");
+                    System.out.print("Enter product name: ");
+                    String name = in.next();
+                    System.out.println("\n-------------------------\n");
+                    Product p = new Product(id, name);
+                    con.addProduct(p);
+                    break;
+                case 2:
+                    System.out.println("\n-------------------------\n");
+                    System.out.print("Enter product ID: ");
+                    int numberID = in.nextInt();
+                    System.out.println("\n-------------------------\n");
+                    con.removeProduct(numberID);
+                    break;
+                case 3:
+                    System.out.println("\n-------------------------\n");
+                    System.out.print("Enter product ID: ");
+                    int numberID2 = in.nextInt();
+                    System.out.println("\n-------------------------\n");
+                    con.localizeProduct(numberID2);
+                    break;
+                case 4:
+                    System.out.println("\n-------------------------\n");
+                    con.listProducts();
+                case 5:
+                    running = false;
+                    System.out.println("\n-------------------------\n");
+                    System.out.println("Exiting system...");
+                    System.out.println("Finish");
+                    break;
+            }
+
+        }
+        in.close();
+    }
+}
